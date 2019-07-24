577ca33a74dbe84fccff112d5fa395718922557c
Rσηιqυє R <shadowrebornrz@gmail.com>
2019-07-23 23:56:31 -0400

    Ver 2.5 Done a proper splashscreen

--- app/src/main/java/com/example/blyoutube/Splash.java
+++ app/src/main/java/com/example/blyoutube/Splash.java
@@ -8,33 +8,13 @@ import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
 
-    private Handler mHandler;
-    private Runnable mRunnable;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
-        mRunnable = new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            }
-        };
-
-        mHandler = new Handler();
-
-        mHandler.postDelayed(mRunnable, 2000);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        if (mHandler != null && mRunnable != null){
-            mHandler.removeCallbacks(mRunnable);
-        }
-    }
-
 }
 
